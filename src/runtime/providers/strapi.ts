745c324be8682f498b3faf51e010b2937dc3e1fe
Thilo Hettmer <thilo.hettmer@gmail.com>
2024-11-30 10:02:03 +0100

    Refactor strapi provider, add modifiers.formats attribute

--- src/runtime/providers/strapi.ts
+++ src/runtime/providers/strapi.ts
@@ -1,20 +1,44 @@
-import { withBase, withoutLeadingSlash } from 'ufo'
-import type { ProviderGetImage } from '../../module'
+import { withBase } from 'ufo'
+import type { ProviderGetImage, ResolvedImage } from '../../module'
 
-// https://strapi.io/documentation/developer-docs/latest/development/plugins/upload.html#upload
+export const getImage: ProviderGetImage = (
+  src: string,
+  { modifiers, baseURL = 'http://localhost:1337/uploads' } = {},
+): ResolvedImage => {
+  const breakpoint
+    = modifiers?.breakpoint ?? undefined
+  const formats = modifiers?.formats
 
-export const getImage: ProviderGetImage = (src, { modifiers, baseURL = 'http://localhost:1337/uploads' } = {}) => {
-  const breakpoint = modifiers?.breakpoint ?? ''
+  const path = src.startsWith('/uploads/')
+    ? src.slice(9)
+    : src
 
-  if (!breakpoint) {
+  if (!breakpoint || !formats) {
     return {
-      url: withBase(src, baseURL),
+      url: withBase(path, baseURL),
+    }
+  }
+
+  const breakpoints = ['large', 'medium', 'small', 'thumbnail'] as const
+
+  const startIndex = breakpoints.indexOf(breakpoint)
+
+  for (let i = startIndex; i < breakpoints.length; i++) {
+    const size = breakpoints[i]
+    const format = formats[size as typeof breakpoints[number]]
+
+    if (format?.url) {
+      const formatPath = format.url.startsWith('/uploads/')
+        ? format.url.slice(9)
+        : format.url
+
+      return {
+        url: withBase(formatPath, baseURL),
+      }
     }
   }
 
   return {
-    url: withBase(`${breakpoint}_${withoutLeadingSlash(src)}`, baseURL),
+    url: withBase(path, baseURL),
   }
 }
-
-export const validateDomains = true
